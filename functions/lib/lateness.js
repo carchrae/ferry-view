641c78bd181c235c4049529174d9687cfb42d138
Tom Carchrae <tom@intellecti.ca>
2026-04-29 12:27:22 -0700

    need to init

--- functions/lib/lateness.js
+++ functions/lib/lateness.js
@@ -1,5 +1,8 @@
+
+import admin from 'firebase-admin'
 import { getFirestore, FieldValue } from 'firebase-admin/firestore'
 
+admin.initializeApp()
 const db = getFirestore()
 
 import { sendPushNotification } from './notify.js'
