43a3dc22df8d2bca0ccde49b4053b055ef9cff33
Tom Carchrae <tom@intellecti.ca>
2026-05-17 10:27:10 -0700

    fix firebase config

--- quasar.config.js
+++ quasar.config.js
@@ -13,7 +13,7 @@ export default defineConfig((ctx) => {
       // rawEsbuildEslintOptions: {},
       // rawWebpackEslintPluginOptions: {},
       warnings: true,
-      errors: true
+      errors: true,
     },
 
     // https://v2.quasar.dev/quasar-cli-webpack/prefetch-feature
@@ -22,14 +22,10 @@ export default defineConfig((ctx) => {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-webpack/boot-files
-    boot: [
-      'firebase',
-    ],
+    boot: ['firebase'],
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#css
-    css: [
-      'app.scss'
-    ],
+    css: ['app.scss'],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
@@ -58,8 +54,8 @@ export default defineConfig((ctx) => {
       // webpackTranspileDependencies: [],
 
       esbuildTarget: {
-        browser: [ 'es2022', 'firefox115', 'chrome115', 'safari14' ],
-        node: 'node20'
+        browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
+        node: 'node20',
       },
 
       // rtl: true, // https://quasar.dev/options/rtl-support
@@ -72,13 +68,26 @@ export default defineConfig((ctx) => {
 
       // https://v2.quasar.dev/quasar-cli-webpack/handling-webpack
       // "chain" is a webpack-chain object https://github.com/sorrycc/webpack-chain
-      // chainWebpack (/* chain, { isClient, isServer } */) {}
+      chainWebpack(chain, { isClient, isServer }) {
+        if (process.env.PRODUCTION !== 'true') {
+          chain.plugin('html-webpack').tap((args) => {
+            args[0].productName = 'Bowen Lift (Test Version)'
+            return args
+          })
+        }
+      },
+
+      env: {
+        VAPID_PUBLIC_KEY: process.env.VAPID_PUBLIC_KEY || '',
+        DEV: ctx.dev,
+        PRODUCTION: process.env.PRODUCTION === 'true',
+      },
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#devserver
     devServer: {
       server: {
-        type: 'http'
+        type: 'http',
       },
       port: 9000,
       open: true,
@@ -87,9 +96,9 @@ export default defineConfig((ctx) => {
           context: ['/api'],
           target: 'https://bowenferry.ca',
           changeOrigin: true,
-          pathRewrite: { '^/api': '/Production' }
-        }
-      ]
+          pathRewrite: { '^/api': '/Production' },
+        },
+      ],
     },
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#framework
@@ -107,7 +116,7 @@ export default defineConfig((ctx) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['Dialog']
+      plugins: ['Dialog'],
     },
 
     // animations: 'all', // --- includes all animations
@@ -131,10 +140,10 @@ export default defineConfig((ctx) => {
     // https://v2.quasar.dev/quasar-cli-webpack/developing-ssr/configuring-ssr
     ssr: {
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       middlewares: [
-        'render' // keep this as last one
+        'render', // keep this as last one
       ],
 
       // extendPackageJson (json) {},
@@ -147,7 +156,7 @@ export default defineConfig((ctx) => {
 
       pwa: {
         injectPwaMetaTags: true,
-      }
+      },
       // pwaOfflineHtmlFilename: 'offline.html', // do NOT use index.html as name!
 
       // pwaExtendGenerateSWOptions (cfg) {},
@@ -156,7 +165,7 @@ export default defineConfig((ctx) => {
 
     // https://v2.quasar.dev/quasar-cli-webpack/developing-pwa/configuring-pwa
     pwa: {
-      workboxMode: 'GenerateSW' // 'GenerateSW' or 'InjectManifest'
+      workboxMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       // swFilename: 'sw.js',
       // manifestFilename: 'manifest.json',
       // extendManifestJson (json) {},
@@ -174,7 +183,7 @@ export default defineConfig((ctx) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/developing-capacitor-apps/configuring-capacitor
     capacitor: {
-      hideSplashscreen: true
+      hideSplashscreen: true,
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/developing-electron-apps/configuring-electron
@@ -185,7 +194,7 @@ export default defineConfig((ctx) => {
       // extendPackageJson (json) {},
 
       // Electron preload scripts (if any) from /src-electron, WITHOUT file extension
-      preloadScripts: [ 'electron-preload' ],
+      preloadScripts: ['electron-preload'],
 
       // specify the debugging port to use for the Electron app when running in development mode
       inspectPort: 5858,
@@ -194,13 +203,11 @@ export default defineConfig((ctx) => {
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
         // OS X / Mac App Store
         // appBundleId: '',
         // appCategoryType: '',
         // osxSign: '',
         // protocol: 'myapp://path',
-
         // Windows only
         // win32metadata: { ... }
       },
@@ -208,8 +215,8 @@ export default defineConfig((ctx) => {
       builder: {
         // https://www.electron.build/configuration
 
-        appId: 'bowen-lift-ferry'
-      }
+        appId: 'bowen-lift-ferry',
+      },
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/developing-browser-extensions/configuring-bex
@@ -225,7 +232,7 @@ export default defineConfig((ctx) => {
        *
        * @example [ 'my-script.ts', 'sub-folder/my-other-script.js' ]
        */
-      extraScripts: []
-    }
+      extraScripts: [],
+    },
   }
 })
--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,16 +1,49 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getMessaging } from 'firebase/messaging'
+import { getAnalytics, isSupported } from 'firebase/analytics'
 
-const firebaseConfig = {
+const stagingConfig = {
+  apiKey: 'AIzaSyCEofI4Nj30jo3fbWjjWKd6Pzrehj768vs',
+  authDomain: 'bowen-ferry-staging.firebaseapp.com',
+  projectId: 'bowen-ferry-staging',
+  storageBucket: 'bowen-ferry-staging.firebasestorage.app',
+  messagingSenderId: '118448121098',
+  appId: '1:118448121098:web:af5975ff6809145e3706f3',
+}
+
+const prodConfig = {
   apiKey: 'AIzaSyA_MuEMwhIi0khDWk7vvWL4oszi7kBWHsI',
   authDomain: 'bowen-ferry.firebaseapp.com',
   projectId: 'bowen-ferry',
   storageBucket: 'bowen-ferry.firebasestorage.app',
   messagingSenderId: '369584658317',
   appId: '1:369584658317:web:4feddf4cbd3841fde78fc4',
+  measurementId: 'G-Z4JWMNK727',
 }
+
+let productionEnv
+
+try {
+  productionEnv = process.env.PRODUCTION
+} catch (e) {
+  console.log('fail to get env', e)
+}
+const isProduction = productionEnv === 'true'
+
+const firebaseConfig = isProduction ? prodConfig : stagingConfig
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+export const messaging = getMessaging(app)
+
+export let analytics = null
+if (isProduction) {
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app)
+    }
+  })
+}
